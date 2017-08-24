d9ac1312cfcba44c3a6ad04cd4e3216ee33dd427
DESKTOP-0PEK566\EdLaptop <e_brown18@hotmail.co.uk>
2017-08-24 09:56:53 +0100

    3) BRWNETheme latest

--- header.php
+++ header.php
@@ -13,10 +13,6 @@
     <!-- Custom styles for this template -->
 
 <link href="<?php echo get_bloginfo( 'template_directory' );?>/style.css" rel="stylesheet">
-<link href="<?php echo get_bloginfo( 'template_directory' );?>/style-hacks.css" rel="stylesheet">
-<link href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/reg_style.css" rel="stylesheet">
-<link href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/profile_style.css" rel="stylesheet">
-<link href="<?php echo get_bloginfo( 'template_directory' );?>/assets/css/messenger_style.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open Sans">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="<?php echo get_bloginfo( 'template_directory' );?>/assets/js/jQuery3.2.1.js"></script>
@@ -24,131 +20,64 @@
 <script src="<?php echo get_bloginfo( 'template_directory' );?>/assets/js/parallax.js"></script>
 <script type="text/javascript" src="http://localhost/wp-content/plugins/BRWNETextWidgetPlugin/hide-show.js"></script>
 
-    <style>
-        html,body{height:100%;}
-        .wrapper{min-height:100%; position:relative}
-        .full{position:absolute; top:0; left:0; width:100%; height:100%;}
-    </style>
-      
-    <script>         
-    function noPasteAlert() {
-    document.getElementById("alert").style.display='block';
-    }
-    </script>
-
-    <script>
+<script>
     $(document).ready(function() {
-        console.log( "document loaded" );
-        
-        $("#scrollButton").click(function scrollContent() {
-            /* var x = document.getElementById("middle"); */
-            /* x.scrollIntoView({ block: "start", behavior: "smooth" }); */
-            $('html, body').animate({scrollTop: $('#middle').offset().top -50 }, 'slow');
-        });
-    });
-    </script>
-      
-    <script>
-    $(document).ready(function(){
-      refreshTable();
-    });
-
-    function refreshTable(){
-        $(".messengerscript").load("/wp-content/themes/BRWNETheme/includes/messenger_script.php", function( response, status, xhr ) {
-              if ( status == "error" ) {
-                var msg = 'Loading messenges resulted in an error: ';
-                console.log(msg + xhr.status + " " + xhr.statusText );
-              } else {
-                setTimeout(refreshTable, 4000);
-                console.log('messenger loader is ticking every 5 seconds')
-                /* displayNotification($('#messenger_script')); */
-              }
-        });
-    }
-    </script>
-<!--
-<script type="text/javascript">// <![CDATA[
-$(document).ready(function() {
-$.ajaxSetup({ cache: false }); // This part addresses an IE bug. without it, IE will only load the first number and will never refresh
-setInterval(function() {
-$('.messenger_script').load('messenger_script.php');
-}, 2000); // the "3000" here refers to the time to refresh the div. it is in milliseconds.
-});
-// ]]></script>
--->              
-   <script>
-        $(document).ready(function() {
-            if($(".topNavMenu").offset().top === 0 && $(window).width() > 768 && top.location.pathname === '/') {
+        if($(".topNavMenu").offset().top === 0 && $(window).width() > 768 && top.location.pathname === '/') {
             $(".topNavMenu").css("background-color", "transparent");
-            }
-            else {
-                $(".topNavMenu").css("background-color", "#333");
-            } 
-        });
-
-        $(document).scroll(function() {
-            if($(".topNavMenu").offset().top === 0 && $(window).width() > 768 && top.location.pathname === '/') {
-            $(".topNavMenu").css("background-color", "transparent");
-            }
-            else {
-                $(".topNavMenu").css("background-color", "#333");
-            }
-        });
-        $(window).resize(function() {
-            if($(".topNavMenu").offset().top === 0 && $(window).width() > 768 && top.location.pathname === '/') {
-            $(".topNavMenu").css("background-color", "transparent");
-            }
-            else {
-                $(".topNavMenu").css("background-color", "#333");
-            }
-        });  
-    </script>
-      
-        <?php  
-        if(isset($_SESSION['username'])) {
-            echo "<script>document.getElementById('logout').style.display='block';</script>";
-            //document.getElementById("logout").style.display='block';
-            console.log('logout link display is active');
-            echo "<script>console.log('logout link display is active');</script>";
         }
         else {
-            echo "<script>document.getElementById('logout').style.display='none';</script>";
-            //document.getElementById("logout").style.display='none';
-            echo "<script>console.log('logout link is disabled');</script>";
+             $(".topNavMenu").css("background-color", "#333");
+        } 
+    });
+
+    $(document).scroll(function() {
+       if($(".topNavMenu").offset().top === 0 && $(window).width() > 768 && top.location.pathname === '/') {
+            $(".topNavMenu").css("background-color", "transparent");
         }
-        ?>
+        else {
+            $(".topNavMenu").css("background-color", "#333");
+        }
+    });
+    
+    $(window).resize(function() {
+        if($(".topNavMenu").offset().top === 0 && $(window).width() > 768 && top.location.pathname === '/') {
+            $(".topNavMenu").css("background-color", "transparent");
+        }
+        else {
+            $(".topNavMenu").css("background-color", "#333");
+        }
+    });  
+</script>
     
 <!--
-        <script>
-        jQuery( document ).ready(function() {
-        console.log( "document loaded" );
-            jQuery(".parallax-title-text").on("swipe", scrollContent(swipeleft) {
-                jQuery('html, body').animate({scrollTop: jQuery('#middle').offset().top-50}, 'slow');
-            });                                         
-        });
-        
-      </script>
+<script>
+    jQuery( document ).ready(function() {
+    console.log( "document loaded" );
+        jQuery(".parallax-title-text").on("swipe", scrollContent(swipeleft) {
+            jQuery('html, body').animate({scrollTop: jQuery('#middle').offset().top-50}, 'slow');
+        });                                         
+    });
+</script>
 -->
-
-      
+  
       
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-	<?php wp_head();?>
+      
+<?php wp_head();?>
 
-  </head>
-
+</head>
 <body>
 
-    <div>
-<div class="topNavMenu" id="wp_TopNavMenu">
-    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+<div>
+    <div class="topNavMenu" id="wp_TopNavMenu">
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
         <a href="javascript:void(0);" class="icon" onclick="switchNavCss()">&#9776;</a>
-</div>
     </div>
+</div>
 
 <?php
 $defaultTitle = get_bloginfo( 'name' );
--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php /* Template Name: Log out Template */ ?>
-
-<?php
-require 'config/config.php';
-
-session_destroy();
-header("Location: /register");
-exit();
-?>
